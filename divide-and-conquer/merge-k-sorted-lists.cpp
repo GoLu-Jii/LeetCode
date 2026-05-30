7d709adeb9aff8af0c3b557f2a69c4f641a2c23d
Gaurav Joshi <gauravj121232@gmail.com>
2026-05-30 09:19:08 +0530

    LC-merge-k-sorted-lists [Runtime: N/A; Memory: N/A]

---
+++ divide-and-conquer/merge-k-sorted-lists.cpp
@@ -0,0 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+struct Compare {
+    bool operator()(ListNode* a, ListNode* b) {
+        return a->val > b->val;  // min-heap based on node value
+    }
+};
+
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        priority_queue<ListNode*, vector<ListNode*>, Compare> pq;
+
+        for (auto item : lists) {
+            while (item) {
+                pq.push(item);
+                item = item->next;   // missing
+            }
+        }
+
+        ListNode* head = new ListNode(0);
+        ListNode* temp = head;
+
+        while (!pq.empty()) {
+            ListNode* node = pq.top();
+            pq.pop();
+
+            temp->next = node;
+            temp = temp->next;
+        }
+
+        temp->next = nullptr;  // good practice
+
+        return head->next;
+    }
+};
