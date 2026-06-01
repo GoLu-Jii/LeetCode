6f1b931de1e26c5c7bea7f06793e264d78b0d132
Gaurav Joshi <gauravj121232@gmail.com>
2026-06-01 08:20:58 +0530

    LC-jump-game-ii [Runtime: N/A; Memory: N/A]

--- dynamic-programming/jump-game-ii.cpp
+++ dynamic-programming/jump-game-ii.cpp
@@ -1,17 +1,29 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
-        int i = 0;
-        int j = nums.size()- 1;
-        int maxjump = 0;
-        while(j != 0  ){
-            if(i + nums[i] >= j){
-                j = i;
-                maxjump++;
-            }
-            else i++;
-        }
-        return maxjump;
+        // int i = 0;
+        // int j = nums.size()- 1;
+        // int maxjump = 0;
+        // while(j != 0  ){
+        //     if(i + nums[i] >= j){
+        //         j = i;
+        //         maxjump++;
+        //     }
+        //     else i++;
+        // }
+        // return maxjump;
         //-------------------------> TLE
+
+        int currmax = 0;
+        int maxi = 0;
+        int jump = 0;
+        for(int i = 0; i < nums.size() - 1; i++){
+            maxi = max(maxi, i + nums[i]);
+            if(i == currmax){
+                jump++;
+                currmax = maxi;
+            }
+        }
+        return jump;
     }
 };
